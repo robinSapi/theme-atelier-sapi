9f46e3d47d66c15b078fc1963b7a33b4271c8b1d
Sapi <sapi@Sirius.local>
2026-03-26 16:46:16 +0100

    Revert "fix(reviews): Troncature avis à 20 mots"
    This reverts commit f65aba209c2f10f6b2c14bcef00e03cedbf3d54e.

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -743,11 +743,10 @@ get_header();
         </div>
         <?php
           $text = $review['text'];
-          $words = explode(' ', $text);
-          if (count($words) > 20) {
-            $short = implode(' ', array_slice($words, 0, 20)) . '…';
-          } else {
-            $short = $text;
+          $short = $text;
+          if (mb_strlen($text) > 200) {
+            $short = mb_substr($text, 0, 200);
+            $short = mb_substr($short, 0, mb_strrpos($short, ' ')) . '…';
           }
         ?>
         <p class="testimonial-text"><?php echo esc_html($short); ?></p>
